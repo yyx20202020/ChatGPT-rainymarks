3d81b1374bc9946f361a9d12ffae5709c09b1120
yyx20202020 <126185233+yyx20202020@users.noreply.github.com>
2023-04-05 16:00:45 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -157,7 +157,7 @@ function _Home() {
               />
             </div>
             <div className={styles["sidebar-action"]}>
-              <a href={REPO_URL} target="_blank">
+              <a href="aiplus.wiki" target="_blank">
                 <IconButton icon={<GithubIcon />} shadow />
               </a>
             </div>
